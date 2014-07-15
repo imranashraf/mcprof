74c9e07ed014ea8d3ba7b4d0d37aad5673e6a384
Imran Ashraf <iimran.aashraf@gmail.com>
2014-07-15 12:58:08 +0200

    pintrace.cpp changed to C++

--- pintrace.cpp
+++ pintrace.cpp
@@ -1,49 +1,39 @@
-
-/*! @file
- *  This is an example of the PIN tool that demonstrates some basic PIN APIs
- *  and could serve as the starting point for developing your first PIN tool
+/*
+ *  This file contains an ISA-portable PIN tool for tracing memory accesses.
  */
 
-#include "pin.H"
-#include "globals.h"
-#include "pintrace.h"
 #include <iostream>
 #include <fstream>
+
+#include "pin.H"
 
 /* ================================================================== */
 // Global variables
 /* ================================================================== */
-
-UINT64 insCount = 0;        //number of dynamically executed instructions
-UINT64 bblCount = 0;        //number of dynamically executed basic blocks
-UINT64 threadCount = 0;     //total number of threads, including main thread
-
-std::ostream * out = &cerr;
+// std::ostream * fout = &cerr;
+std::ofstream fout;
 
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-                            "o", "", "specify file name for MyPinTool output");
+                            "o", "memtrace.out", "specify file name for MyPinTool output");
 
-KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
-                       "count", "1", "count instructions, basic blocks and threads in the application");
-
-
+KNOB<BOOL>   KnobMemTrace(KNOB_MODE_WRITEONCE,  "pintool",
+                       "trace", "1", "trace memory accesses in the application");
 /* ===================================================================== */
 // Utilities
 /* ===================================================================== */
 
-/*!
- *  Print out help message.
- */
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
 INT32 Usage()
 {
-    cerr << "This tool prints out the number of dynamically executed " << endl <<
-         "instructions, basic blocks and threads in the application." << endl << endl;
-
+    cerr << "This Pintool prints a trace of memory addresses"<<endl;
     cerr << KNOB_BASE::StringKnobSummary() << endl;
-
+    
     return -1;
 }
 
@@ -51,71 +41,77 @@ INT32 Usage()
 // Analysis routines
 /* ===================================================================== */
 
-/*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
+// Print a memory read record
+VOID RecordMemRead(VOID * ip, VOID * addr, UINT32 refSize)
 {
-    bblCount++;
-    insCount += numInstInBbl;
+    fout << ip << " " << addr << " R " << refSize << "\n"; 
+}
+
+// Print a memory write record
+VOID RecordMemWrite(VOID * ip, VOID * addr, UINT32 refSize)
+{
+    fout << ip << " " << addr << " W " << refSize << "\n"; 
 }
 
 /* ===================================================================== */
 // Instrumentation callbacks
 /* ===================================================================== */
 
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block
- * of the trace.
- * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
- * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
- *                       function call
- */
-VOID Trace(TRACE trace, VOID *v)
+// Is called for every instruction and instruments reads and writes
+VOID Instruction(INS ins, VOID *v)
 {
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
+    // Instruments memory accesses using a predicated call, i.e.
+    // the instrumentation is called iff the instruction will actually be executed.
+    //
+    // On the IA-32 and Intel(R) 64 architectures conditional moves and REP 
+    // prefixed instructions appear as predicated instructions in Pin.
+    UINT32 memOperands = INS_MemoryOperandCount(ins);
+
+    // Iterate over each memory operand of the instruction.
+    for (UINT32 memOp = 0; memOp < memOperands; memOp++)
+    {
+        UINT32 refSize = INS_MemoryOperandSize(ins, memOp);
+
+        if (INS_MemoryOperandIsRead(ins, memOp))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_UINT32, refSize,
+                IARG_END);
+        }
+        // Note that in some architectures a single memory operand can be 
+        // both read and written (for instance incl (%eax) on IA-32)
+        // In that case we instrument it once for read and once for write.
+        if (INS_MemoryOperandIsWritten(ins, memOp))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_UINT32, refSize,
+                IARG_END);
+        }
     }
 }
 
+/* ===================================================================== */
+/* Fini                                                                  */
+/* ===================================================================== */
 /*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
-{
-    threadCount++;
-}
-
-/*!
- * Print out analysis results.
- * This function is called when the application exits.
- * @param[in]   code            exit code of the application
- * @param[in]   v               value specified by the tool in the
- *                              PIN_AddFiniFunction function call
+ * The Fini procedure of the tool.
+ * This function is called at the end of the application.
  */
 VOID Fini(INT32 code, VOID *v)
 {
-    *out <<  "===============================================" << endl;
-    *out <<  "MyPinTool analysis results: " << endl;
-    *out <<  "Number of instructions: " << insCount  << endl;
-    *out <<  "Number of basic blocks: " << bblCount  << endl;
-    *out <<  "Number of threads: " << threadCount  << endl;
-    *out <<  "===============================================" << endl;
+    fout <<  "#eof"<<endl;
+    fout.close();
 }
 
+/* ===================================================================== */
+/* SetupPin                                                              */
+/* ===================================================================== */
 /*!
  * The main procedure of the tool.
  * This function is called when the application image is loaded but not yet started.
@@ -125,31 +121,23 @@ VOID Fini(INT32 code, VOID *v)
  */
 void SetupPin(int argc, char *argv[])
 {
-    // Initialize PIN library. Print help message if -h(elp) is specified
-    // in the command line or the command line is invalid
-    if( PIN_Init(argc,argv) ) {
-        Usage();
-        return;
+    // Initialize symbol processing
+    PIN_InitSymbols();
+    
+    if (PIN_Init(argc, argv)){
+      Usage();
+      return;
     }
 
     string fileName = KnobOutputFile.Value();
-
     if (!fileName.empty()) {
-        out = new std::ofstream(fileName.c_str());
+        fout.open(fileName.c_str()); 
+        if(fout.fail()){
+            cerr << "Error Opening File" << endl;
+            return;
+        }
     }
-
-    if (KnobCount) {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
-
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
-
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
+    
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
 }
-
-/* ===================================================================== */
-/* eof */
-/* ===================================================================== */
